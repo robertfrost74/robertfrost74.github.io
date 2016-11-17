466befd4ac8d0dc145519f0591ec7e80d50e72f6
robertfrost74 <robfro@me.com>
2016-11-17 22:18:02 +0100

    Arrow to top

--- js/init.js
+++ js/init.js
@@ -3,6 +3,16 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $(window).scroll(function(){
+    var scrolled = $(window).scrollTop();
+    if (scrolled > 200) $('.top').fadeIn('slow');
+    if (scrolled < 200) $('.top').fadeOut('slow');
+  });
+  
+  //Click event
+  $('.top').click(function () {
+    $("html, body").animate({ scrollTop: "0" },  500);
+  });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
